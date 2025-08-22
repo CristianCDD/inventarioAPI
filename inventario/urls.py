ede30e5106dc14310802d8994bedc76ca882c096
Cristian Damian <cristian10damian@gmail.com>
2025-08-22 18:49:12 -0500

    listo para render

--- inventario/urls.py
+++ inventario/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Ruta para el panel de administración
-    path('api/v1/', include('api.urls')),  # Ruta para la API, incluir las URLs de la aplicación 'api'
+    path('', include('api.urls')),  # Ruta para la API, incluir las URLs de la aplicación 'api'
 ]
